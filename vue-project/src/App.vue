8c6cc18950c1ee650fb61ba6aed4710ed1114f6c
oleskarr <olesastankevic7@gmail.com>
2024-08-27 19:21:39 +0300

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Проект на Vue.js</h1>
+  <button @click = 'counter++'>Счётчик {{ counter }}</button>
 </template>
+
+<script setup>
+import {ref} from 'vue'
+const counter = ref(0)
+const counter2 = 0
+
+</script>
