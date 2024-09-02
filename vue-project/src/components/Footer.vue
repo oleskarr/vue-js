f535022763570f6f36a80836449e43a85739ddf4
oleskarr <olesastankevic7@gmail.com>
2024-09-02 22:09:50 +0300

    обновления

--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -1,26 +1,59 @@
 <template>
-     <footer>
-  <div class="share">
-    <img src="..\assets\img\vkontacte.png" >
-    <img src="..\assets\img\odnoklassniki 1.png" >
-    <img src="..\assets\img\telegram.png" >
-  </div>
-  <p>Поделиться</p>
-</footer>
+  <footer :style="{ height: footerHeight }" @click="toggleHeight">
+   
+    <div class="share">
+      <img src="../assets/img/vkontacte.png" />
+      <img src="../assets/img/odnoklassniki 1.png" />
+      <img src="../assets/img/telegram.png" />
+    </div>
+    <p>Поделиться</p>
+  </footer>
 </template>
-<style>
-footer{
-    background-color: #B54B62;
-    width: max-content;
-    border-top-left-radius:10px ;
-    border-top-right-radius:10px ;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+
+<style scoped>
+footer p {
+  font-size: 24px;
+  color: #fecfb0;
+  margin-top: 5px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+
 }
-.share{
-    display: flex;
-    gap: 24px;
-    padding: 5px;
+
+footer img {
+  width: 32px;
+  height: 32px;
+}
+
+footer {
+  position: absolute;
+  margin-top: -40px;
+  background-color: #b54b62;
+  width: max-content;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: hidden;
+  margin-left: 50px;
+  height: 40px;
+  transition: height 0.3s ease; 
+ 
+}
+.share {
+  display: flex;
+  gap: 25px;
+  padding: 14px;
 }
 </style>
+
+<script setup>
+import { ref } from 'vue';
+
+const footerHeight = ref('40px'); // Начальная высота
+
+const toggleHeight = () => {
+  footerHeight.value = footerHeight.value === '40px' ? 'max-content' : '40px';
+};
+</script>
--- vue-project/src/components/Main.vue
+++ vue-project/src/components/Main.vue
@@ -1,68 +1,93 @@
 <template>
-    <main>
-<div class="container">
-  <img src="../assets\img\cabbage.png">
-  <p>Капуста давно и прочно вошла в рацион
-нашего питания и принадлежит к числу
-важнейших овощных растений. Она
-также является неотъемлемой частью
-таких традиционных блюд, как: борщ,
-щи, солянка, тушеная капуста, голубцы,
-капустная запеканка и конечно
-квашеная капуста.</p>
-<p style="width: 907px;">Капуста не только популярный, но и полезный продукт: она богата
-витаминами А, В1, Р, К, В6, U. Капуста является лидером среди овощей по
-содержанию витамина С, причем он сохраняется в капусте как при длительном
-хранении в сыром виде, так и при термической обработке. В ней также много
-минералов - сера, кальций, калий и фосфор, клетчатки и воды.</p>
-</div>
-<div class="likes">
-  <img src="..\assets\img\stayble.png" class="f">
-  <img src="..\assets\img\cursor-on.png" class="s">
-  <img src="..\assets\img\liked.png" class="th" @click="toBlock">
-</div>
+  <main>
+    <div class="container">
+      <img src="../assets/img/cabbage.png" />
+      <p>
+        Капуста давно и прочно вошла в рацион нашего питания и принадлежит к
+        числу важнейших овощных растений. Она также является неотъемлемой частью
+        таких традиционных блюд, как: борщ, щи, солянка, тушеная капуста,
+        голубцы, капустная запеканка и конечно квашеная капуста.
+      </p>
+      <p style="width: 907px;">
+        Капуста не только популярный, но и полезный продукт: она богата
+        витаминами А, В1, Р, К, В6, U. Капуста является лидером среди овощей по
+        содержанию витамина С, причем он сохраняется в капусте как при
+        длительном хранении в сыром виде, так и при термической обработке. В ней
+        также много минералов - сера, кальций, калий и фосфор, клетчатки и воды.
+      </p>
+    </div>
+    <div class="likes">
+      <!-- Картинка по умолчанию -->
+      <img src="../assets/img/stayble.png" class="f" />
 
-</main>
+      <!-- Картинка при наведении -->
+      <img src="../assets/img/cursor-on.png" class="s" />
+
+      <!-- Картинка при клике -->
+      <img
+        src="../assets/img/liked.png"
+        class="th"
+        @click="toBlock"
+        :class="{ show: isClicked }"
+      />
+    </div>
+  </main>
 </template>
+
 <style>
-.container{
+main{
+  position: relative;
+}
+.container {
   display: flex;
   gap: 10px;
   flex-wrap: wrap;
   padding: 30px;
   align-items: center;
-  p{
-    width: 461px;
-    height: 259px;
-    font-size: 24px;
-    color: #B54B62;
-  }
-}
-.likes{
-    display: flex;
-    gap: 0;
-    width: max-content;
-}
-.likes .s{
-    display: none;
-}
-.likes .th{
-    display: none;
-}
-.likes:hover .f{
-   display: none;
-}
-.likes:hover .s{
-   display: block;
 }
 
-</style>
-<script setup>
-import {ref, reactive } from 'vue';
-const toBlock = function(){
- block.blocked=ref('true')
+.container p {
+  width: 461px;
+  height: 259px;
+  font-size: 24px;
+  color: #b54b62;
 }
-const block = reactive({
-    blocked :'display:block'
-})
+
+.likes {
+  display: flex;
+  gap: 0;
+  width: max-content;
+}
+
+.likes .s {
+  display: none;
+}
+
+.likes .th {
+  display: none; /* Изначально скрываем третий элемент */
+}
+
+.likes .th.show {
+  display: block; /* Показать элемент, когда класс "show" добавлен */
+}
+
+.likes:hover .f {
+  display: none;
+}
+
+.likes:hover .s {
+  display: block;
+}
+</style>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const isClicked = ref(false);
+
+// Функция для переключения стиля
+const toBlock = () => {
+  isClicked.value = !isClicked.value; // Переключаем состояние
+};
 </script>
